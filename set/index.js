602bff675c6b5753dc65d6827cb34a51d19d88cb
Oleg Myasnikov <moa@htc-cs.com>
2018-04-22 18:27:23 +0400

    Add Set

--- set/index.js
+++ set/index.js
@@ -1,90 +1,125 @@
 class Set {
-    /**
-     * Создает сет, опционально принимая элементы для добавления
-     * @param {...*} [items] Добавляемые элементы
-     */
-    constructor(...items) {
-        this.items = items;
+  /**
+   * Создает сет, опционально принимая элементы для добавления
+   * @param {...*} [items] Добавляемые элементы
+   */
+  constructor(...items) {
+    this.items = [];
+
+    items.forEach(item => {
+      this.add(item);
+    });
+  }
+
+  /**
+   * Возвращает количество элементов в сете
+   * @returns {number}
+   */
+  get size() {
+    return this.items.length;
+  }
+
+  /**
+   * Возвращает массив элементов сета
+   * @returns {Array}
+   */
+  get values() {
+    return this.items;
+  }
+
+  /**
+   * Добавляет элемент в сет
+   * @param {*} item
+   */
+  add(item) {
+    if (!this.has(item)) {
+      this.items.push(item);
     }
+  }
 
-    /**
-     * Возвращает количество элементов в сете
-     * @returns {number}
-     */
-    get size() {
-        return this.items.length;
-    }
+  /**
+   * Проверяет наличие элемента в сете
+   * @param {*} item
+   * @returns {boolean}
+   */
+  has(item) {
+    return this.values.includes(item);
+  }
 
-    /**
-     * Возвращает массив элементов сета
-     * @returns {Array}
-     */
-    get values() {
-        return this.items;
-    }
+  /**
+   * Удаляет элемент из сета и возвращает `true` если элемент удален и `false` если нет
+   * @param {*} item
+   * @returns {boolean}
+   */
+  remove(item) {
+    const index = this.values.indexOf(item);
 
-    /**
-     * Добавляет элемент в сет
-     * @param {*} item
-     */
-    add(item) {
-        if (this.has(item)) return;
+    if (index === -1) return false;
 
-        this.items.push(item);
-    }
+    this.items.splice(index, 1);
+    return true;
+  }
 
-    /**
-     * Проверяет наличие элемента в сете
-     * @param {*} item
-     * @returns {boolean}
-     */
-    has(item) {
-        return this.items.includes(item);
-    }
+  /**
+   * Удаляет все элементы в сете
+   */
+  clear() {
+    this.items = [];
+  }
 
-    /**
-     * Удаляет элемент из сета и возвращает `true` если элемент удален и `false` если нет
-     * @param {*} item
-     * @returns {boolean}
-     */
-    remove(item) {}
+  /**
+   * Возращает сет состоящий из элементов двух сетов
+   * @param {Set} set
+   * @returns {Set}
+   */
+  union(set) {
+    this.items = new Set(...this.values, ...set.values).values;
+    return this;
+  }
 
-    /**
-     * Удаляет все элементы в сете
-     */
-    clear() {
-        this.items = [];
-    }
+  /**
+   * Возращает сет состоящий из элементов которые присутствуют в обоих сетах
+   * @param {Set} set
+   * @returns {Set}
+   */
+  intersection(set) {
+    const unionArray = new Set(...this.values, ...set.values);
 
-    /**
-     * Возращает сет состоящий из элементов двух сетов
-     * @param {Set} set
-     * @returns {Set}
-     */
-    union(set) {
-        return this.items.concat(set);
-    }
+    this.items = unionArray.values.filter(
+      item => this.has(item) && set.has(item)
+    );
 
-    /**
-     * Возращает сет состоящий из элементов которые присутствуют в обоих сетах
-     * @param {Set} set
-     * @returns {Set}
-     */
-    intersection() {}
+    return this;
+  }
 
-    /**
-     * Возращает сет состоящий из элементов присутствующих в первом сете, и отсутствующих во втором
-     * @param {Set} set
-     * @returns {Set}
-     */
-    difference() {}
+  /**
+   * Возращает сет состоящий из элементов присутствующих в первом сете, и отсутствующих во втором
+   * @param {Set} set
+   * @returns {Set}
+   */
+  difference(set) {
+    const unionArray = new Set(...this.values, ...set.values);
 
-    /**
-     * Возвращает `true` если сет содержит в себе все элементы из друого сета
-     * @param {Set} set
-     * @returns {boolean}
-     */
-    isSubset() {}
+    this.items = unionArray.values.filter(
+      item => this.has(item) && !set.has(item)
+    );
+
+    return this;
+  }
+
+  /**
+   * Возвращает `true` если сет содержит в себе все элементы из друого сета
+   * @param {Set} set
+   * @returns {boolean}
+   */
+  isSubset(set) {
+    if (this.size > set.size) return false;
+
+    return this.values.reduce((curr, item) => {
+      if (curr === false) return false;
+      return set.has(item);
+    }, true);
+  }
 }
 
 module.exports = Set;
